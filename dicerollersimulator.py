a0152416e9589912a940e5d46ff6e5446a8f55a2
John Manuel <92555489+lawabidingcoder@users.noreply.github.com>
2025-05-16 13:43:08 +0100

    Update assignment_one.ipynb

--- dicerollersimulator.py
+++ dicerollersimulator.py
@@ -1,15 +1,17 @@
-# Assignment 1
-# Dice Roller Simulator
-import random # loads the built-in random module 
-
-def roll_the_dice():
-    random_number = random.randint(1, 6) # generates random number
-    print("You rolled:", random_number)  # from given range
-
-# Program Loop
-while True:
-    roll_the_dice()
-    again = input("Do you want to roll again? (yes/no): ")
-    if again != "yes":
-        print("Goodbye!") # Breaks the program if no or if other charcter written 
-        break     
+## Assignment 1
+## Stimulating rolling a dice
+import random
+input("Press enter to roll the dice")
+dice = random.randint(1, 6)
+## The random module is used to generate random numbers
+print("You rolled a " + str(dice))
+## Concatenate the string with the number
+## The str() function is used to convert the number to a string
+answer = input("Do you want to roll again? Yes or No: ")
+## Ask the user if they want to roll again
+while answer.lower() == "yes":
+  dice = random.randint(1, 6)
+  print("You rolled a " + str(dice))
+  answer = input("Do you want to roll again? Yes or No: ")
+## Loop until the user enters "no"
+print("Thank you for playing!")
